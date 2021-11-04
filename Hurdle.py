1048e5e8c6d7da050da20a1e01677b5d3bd4bfda
kaos981 <info2mservizi@gmail.com>
2021-11-04 06:33:09 +0100

    Add files via upload

---
+++ Hurdle.py
@@ -0,0 +1,41 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while wall_on_right() and front_is_clear():
+        move()
+    turn_left()
+
+#Con il for loop    
+#for step in range(6):
+    #jump()
+
+#Con il while loop, opzione 1    
+#robot = True
+#while robot is not at_goal():
+    #jump()
+    
+#Con il while loop, opzione 2    
+#while at_goal() is not True:
+    #jump()
+    
+#Con il while loop, opzione 3
+#while not at_goal():
+#    jump()
+
+ 
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        jump()
+  
+    
